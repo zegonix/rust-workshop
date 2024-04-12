564a4a981ad19c0fd96e1138f70955309188dbca
zegonix <jonas.s97@gmx.ch>
2024-04-12 10:22:22 +0200

    no

--- day_4/badlib/src/lib.rs
+++ day_4/badlib/src/lib.rs
@@ -5,15 +5,18 @@
 //! We will incrementally build a CI pipeline to discover
 //! how CI can help us make sure our Rust code is squeaky clean.
 
+///  DOCUMENTATION
 pub fn badly_formatted() {
     println!("This code is badly formatted.");
     println!("Don't change it until CI fails because of it.");
 }
 
+/// doc help me!
 pub fn clippy_doesnt_like_this() {
     println!("This is ridiculous.");
 }
 
+///  WHY?!
 pub fn dangerous_cast(n: i64) -> i32 {
     // In my opinion, the `as` keyword should be used sparingly.
     // This conversion is lossy and may not do what you expect.
@@ -21,9 +24,10 @@ pub fn dangerous_cast(n: i64) -> i32 {
     // a `Reslut<i64, _>` instead.
     //
     // But as always, let's get CI to complain about it before we fix it.
-    n as i32
+    i32::try_from(n).unwrap_or(0)
 }
 
+/// blab
 pub fn add(left: usize, right: usize) -> usize {
     // I don't know how to add two numbers...
     left - right
